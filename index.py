d0fc7de17f0965e63f6d32a8b407fd5c43ed64ba
BenR001 <benjamin.rosado01@gmail.com>
2022-12-19 23:15:41 -0500

    Added POSTGRES functionality+ Created DB

--- index.py
+++ index.py
@@ -1,5 +1,25 @@
 from flask import Flask  # import objects from flask model
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)  # define app using flask
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:[PASSWORDFORPOSTGRES]@localhost/flaskapp'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model) :
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(80),unique = True)
+    email = db.Column(db.String(120),unique = True)
+
+    def __init__(self,username,email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    
 
 from flask import jsonify, request #added request
 
